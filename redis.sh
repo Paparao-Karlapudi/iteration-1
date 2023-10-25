9a2ac9aebd9585964feff131b51d7c1dd6fca7c8
pappik <pappik@gmail.com>
2023-10-25 11:54:00 -0400

    catalogue itearation

--- redis.sh
+++ redis.sh
@@ -4,4 +4,18 @@ display "Install Redis"
 dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>${LOG}
 statusmsg
 
-display ""
+display "Enable Redis"
+dnf module enable redis:remi-6.2 -y &>>${LOG}
+statusmsg
+
+display "Install Redis"
+dnf install redis -y &>>${LOG}
+statusmsg
+
+display "Update Listening Address"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>${LOG}
+statusmsg
+
+display "Start and Enbale Redis"
+systemctl enable redis &>>${LOG}
+systemctl start redis &>>${LOG}
