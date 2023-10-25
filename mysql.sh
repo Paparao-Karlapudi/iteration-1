cf08e232e65681c2f2090ae1472588a0dc8c67ec
pappik <pappik@gmail.com>
2023-10-25 12:41:40 -0400

    catalogue itearation

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,26 @@
 source common.sh
 
 display "disable previous mysql"
-dnf module disable mysql -y
+dnf module disable mysql -y &>>${LOG}
 statusmsg
 
+display "Copy MySql repo to desired location"
+cp ${configs}/files/mysql.repo /etc/yum.repos.d/mysql.repo &>>${LOG}
+statusmsg
+
+display "Install MySql Server"
+dnf install mysql-community-server -y &>>${LOG}
+statusmsg
+
+display "Enable and Start Mysql"
+systemctl enable mysqld &>>${LOG}
+systemctl start mysqld  &>>${LOG}
+statusmsg
+
+display "Change Root Password for Mysql"
+mysql_secure_installation --set-root-pass RoboShop@1 &>>${LOG}
+statusmsg
+
+display "Check the password updated or not"
+mysql -uroot -pRoboShop@1 &>>${LOG}
+statusmsg
