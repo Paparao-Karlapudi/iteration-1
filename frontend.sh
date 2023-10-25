b8159f827fe75cd7acb57704d4c25e84ae6075f3
pappik <pappik@gmail.com>
2023-10-24 22:08:29 -0400

    frontend

--- frontend.sh
+++ frontend.sh
@@ -1,19 +1,20 @@
+LOG=/tmp/project1.log
 statusmsg()
 {
 if [ $? -eq 0 ]; then
 echo -e "SUCCESS"
 else
   echo -e "\e[31mFAILURE\e[0m"
-  echo "refer project1.log for more details"
+  echo "refer ${LOG} for more details"
   exit 1
 fi
 }
 
 display()
 {
-  echo "\e[36m$1\e[0m"
+  echo -e "\e[35m$1\e[0m"
 }
 
 display "install nginx"
-dnf install nginx -y
+dnf install nginx -y &>>${LOG}
 statusmsg
